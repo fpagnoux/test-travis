4790b7f728aed3dd9bafa7fbba9c484eb3957eb5
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-22 14:37:29 +0200

    Change key chmod

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 version=`cat ./version`
 eval "$(ssh-agent -s)" #start the ssh agent
+chmod 400 ./id_travis-tests
 ssh-add ./id_travis-tests
 ls
 git remote set-url origin git@github.com:fpagnoux/test-travis.git
