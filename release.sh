ea9166353e796ced85b16e05a178ef2d41df4185
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-22 14:00:41 +0200

    Load ssh keu

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 version=`cat ./version`
+eval "$(ssh-agent -s)" #start the ssh agent
+ssh-add ./id_travis-tests
+git remote set-url origin git@github.com:fpagnoux/test-travis.git
 git tag $version
 git push --tags
-echo "Releasing"
