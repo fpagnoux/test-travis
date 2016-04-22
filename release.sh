e2419f58137a15607597a22f081663c381771740
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-22 15:47:18 +0200

    Add pypi credentials

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@ version=`cat ./version`
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 400 ./id_travis-tests
 ssh-add ./id_travis-tests
-ls
 git remote set-url origin git@github.com:fpagnoux/test-travis.git
 git tag $version
 git push --tags
