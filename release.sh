2aea0e0e1e7165f9027e33e31b3fe190d72bcbcb
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-22 14:16:35 +0200

    New attempt

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 version=`cat ./version`
 eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add ./id_travis-tests
+ls
 git remote set-url origin git@github.com:fpagnoux/test-travis.git
 git tag $version
 git push --tags
